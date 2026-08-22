d013d7e2db63ab52cceb1dd2e2f4f90a15c0509d
TrixNEW <cooldinothunder@gmail.com>
2026-08-22 15:32:16 -0400

    rebrand legacy translations

--- src/command/defaults/VersionCommand.php
+++ src/command/defaults/VersionCommand.php
@@ -25,6 +25,7 @@ namespace pocketmine\command\defaults;
 
 use pocketmine\command\CommandSender;
 use pocketmine\lang\KnownTranslationFactory;
+use pocketmine\lang\Translatable;
 use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\permission\DefaultPermissionNames;
 use pocketmine\plugin\Plugin;
@@ -43,8 +44,8 @@ class VersionCommand extends VanillaCommand{
 	public function __construct(){
 		parent::__construct(
 			"version",
-			KnownTranslationFactory::pocketmine_command_version_description(),
-			KnownTranslationFactory::pocketmine_command_version_usage(),
+			KnownTranslationFactory::quark_command_version_description(),
+			KnownTranslationFactory::quark_command_version_usage(),
 			["ver", "about"]
 		);
 		$this->setPermission(DefaultPermissionNames::COMMAND_VERSION);
@@ -52,32 +53,35 @@ class VersionCommand extends VanillaCommand{
 
 	public function execute(CommandSender $sender, string $commandLabel, array $args){
 		if(count($args) === 0){
-			$sender->sendMessage(KnownTranslationFactory::pocketmine_command_version_serverSoftwareName(
-				TextFormat::GREEN . VersionInfo::NAME . TextFormat::RESET
-			));
 			$versionColor = VersionInfo::IS_DEVELOPMENT_BUILD ? TextFormat::YELLOW : TextFormat::GREEN;
-			$sender->sendMessage(KnownTranslationFactory::pocketmine_command_version_serverSoftwareVersion(
-				$versionColor . VersionInfo::VERSION()->getFullVersion() . TextFormat::RESET,
-				TextFormat::GREEN . VersionInfo::GIT_HASH() . TextFormat::RESET
-			));
-			$sender->sendMessage(KnownTranslationFactory::pocketmine_command_version_minecraftVersion(
-				TextFormat::GREEN . ProtocolInfo::MINECRAFT_VERSION_NETWORK . TextFormat::RESET,
-				TextFormat::GREEN . ProtocolInfo::CURRENT_PROTOCOL . TextFormat::RESET
-			));
-			$sender->sendMessage(KnownTranslationFactory::pocketmine_command_version_phpVersion(TextFormat::GREEN . PHP_VERSION . TextFormat::RESET));
 
 			$jitMode = Utils::getOpcacheJitMode();
 			if($jitMode !== null){
 				if($jitMode !== 0){
-					$jitStatus = KnownTranslationFactory::pocketmine_command_version_phpJitEnabled(sprintf("CRTO: %d", $jitMode));
+					$jitStatus = KnownTranslationFactory::quark_command_version_phpJitEnabled(sprintf("CRTO: %d", $jitMode));
 				}else{
-					$jitStatus = KnownTranslationFactory::pocketmine_command_version_phpJitDisabled();
+					$jitStatus = KnownTranslationFactory::quark_command_version_phpJitDisabled();
 				}
 			}else{
-				$jitStatus = KnownTranslationFactory::pocketmine_command_version_phpJitNotSupported();
+				$jitStatus = KnownTranslationFactory::quark_command_version_phpJitNotSupported();
 			}
-			$sender->sendMessage(KnownTranslationFactory::pocketmine_command_version_phpJitStatus($jitStatus->format(TextFormat::GREEN, TextFormat::RESET)));
-			$sender->sendMessage(KnownTranslationFactory::pocketmine_command_version_operatingSystem(TextFormat::GREEN . Utils::getOS() . TextFormat::RESET));
+
+			$this->sendMessages($sender, [
+				KnownTranslationFactory::quark_command_version_serverSoftwareName(
+					TextFormat::GREEN . VersionInfo::NAME . TextFormat::RESET
+				),
+				KnownTranslationFactory::quark_command_version_serverSoftwareVersion(
+					$versionColor . VersionInfo::VERSION()->getFullVersion() . TextFormat::RESET,
+					TextFormat::GREEN . VersionInfo::GIT_HASH() . TextFormat::RESET
+				),
+				KnownTranslationFactory::quark_command_version_minecraftVersion(
+					TextFormat::GREEN . ProtocolInfo::MINECRAFT_VERSION_NETWORK . TextFormat::RESET,
+					TextFormat::GREEN . ProtocolInfo::CURRENT_PROTOCOL . TextFormat::RESET
+				),
+				KnownTranslationFactory::quark_command_version_phpVersion(TextFormat::GREEN . PHP_VERSION . TextFormat::RESET),
+				KnownTranslationFactory::quark_command_version_phpJitStatus($jitStatus->format(TextFormat::GREEN, TextFormat::RESET)),
+				KnownTranslationFactory::quark_command_version_operatingSystem(TextFormat::GREEN . Utils::getOS() . TextFormat::RESET)
+			]);
 		}else{
 			$pluginName = implode(" ", $args);
 			$exactPlugin = $sender->getServer()->getPluginManager()->getPlugin($pluginName);
@@ -98,7 +102,7 @@ class VersionCommand extends VanillaCommand{
 			}
 
 			if(!$found){
-				$sender->sendMessage(KnownTranslationFactory::pocketmine_command_version_noSuchPlugin());
+				$sender->sendMessage(KnownTranslationFactory::quark_command_version_noSuchPlugin());
 			}
 		}
 
@@ -107,25 +111,39 @@ class VersionCommand extends VanillaCommand{
 
 	private function describeToSender(Plugin $plugin, CommandSender $sender) : void{
 		$desc = $plugin->getDescription();
-		$sender->sendMessage(KnownTranslationFactory::pocketmine_command_version_plugin_header(
+		$messages = [KnownTranslationFactory::quark_command_version_plugin_header(
 			TextFormat::DARK_GREEN . $desc->getName() . TextFormat::RESET,
 			TextFormat::DARK_GREEN . $desc->getVersion() . TextFormat::RESET
-		));
+		)];
 
 		if($desc->getDescription() !== ""){
-			$sender->sendMessage($desc->getDescription());
+			$messages[] = $desc->getDescription();
 		}
 
 		if($desc->getWebsite() !== ""){
-			$sender->sendMessage(KnownTranslationFactory::pocketmine_command_version_plugin_website($desc->getWebsite()));
+			$messages[] = KnownTranslationFactory::quark_command_version_plugin_website($desc->getWebsite());
 		}
 
 		if(count($authors = $desc->getAuthors()) > 0){
 			if(count($authors) === 1){
-				$sender->sendMessage(KnownTranslationFactory::pocketmine_command_version_plugin_author(implode(", ", $authors)));
+				$messages[] = KnownTranslationFactory::quark_command_version_plugin_author(implode(", ", $authors));
 			}else{
-				$sender->sendMessage(KnownTranslationFactory::pocketmine_command_version_plugin_authors(implode(", ", $authors)));
+				$messages[] = KnownTranslationFactory::quark_command_version_plugin_authors(implode(", ", $authors));
 			}
 		}
+
+		$this->sendMessages($sender, $messages);
+	}
+
+	/**
+	 * @param list<Translatable|string> $messages
+	 */
+	private function sendMessages(CommandSender $sender, array $messages) : void{
+		$language = $sender->getLanguage();
+		$translated = [];
+		foreach($messages as $message){
+			$translated[] = $message instanceof Translatable ? $language->translate($message) : $message;
+		}
+		$sender->sendMessage(implode("\n", $translated));
 	}
 }
