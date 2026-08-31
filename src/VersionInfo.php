2e7b7a395a609f6a40f6b2f7683f56ec3e687428
IvanCraft623 <ivancraft623@gmail.com>
2026-08-30 23:21:48 -0600

    Prepare 5.48.0 release

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,7 +31,7 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const string NAME = "Quark";
-	public const string BASE_VERSION = "5.47.0";
+	public const string BASE_VERSION = "5.48.0";
 	public const bool IS_DEVELOPMENT_BUILD = false;
 	public const string BUILD_CHANNEL = "stable";
 	public const string GITHUB_URL = "https://github.com/Bedrock-Phanatics/Quark";
